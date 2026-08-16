7e2b63c24ac387cd7d2cff7102d5f150bf0667af
psotoc2021 <patricio.soto06@inacapmail.cl>
2026-08-16 00:19:00 -0400

    security: externalize secrets and add least privilege policy

--- condominio/settings.py
+++ condominio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4f+*x*g#%&e)m8i)^%iu%+5fe&hn9-v5ism@%n_9#d&bslct=l'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-dev-only")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
